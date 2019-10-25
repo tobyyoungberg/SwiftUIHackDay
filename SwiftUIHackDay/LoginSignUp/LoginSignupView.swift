7e414e2ee766535492194047d215c7458d8caf1d
Jayden Garrick <jaydengarrick@Jaydens-MacBook-Pro.local>
2019-10-25 12:05:09 -0600

    add LoginSignupView

---
+++ SwiftUIHackDay/LoginSignUp/LoginSignupView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginSignupView.swift
+//  SwiftUIHackDay
+//
+//  Created by Jayden Garrick on 10/25/19.
+//  Copyright © 2019 T-JAM Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginSignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginSignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginSignupView()
+    }
+}
