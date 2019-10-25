c333e4073e2b6473749c7b32550d9ad5ea330f51
Toby Youngberg <tobyyoungberg@gmail.com>
2019-10-25 15:22:09 -0600

    Added initial implmentation of the MemberHome views

--- SwiftUIHackDay/MemberHome/MemberHomeView.swift
+++ SwiftUIHackDay/MemberHome/MemberHomeView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct MemberHomeView: View {
+    
+    init() {
+    }
+    
     var body: some View {
-        Text("Member Home")
+        NavigationView {
+            ScrollView {
+                VStack {
+                    AccountHeaderView()
+                    MemberFeedCell()
+                    SmallImageMemberFeedCell()
+                    MemberFeedCell()
+                }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                .background(Color("gray-200"))
+            }
+            .navigationBarItems(leading:
+                Button(action: {
+                    print("Profile")
+                }) {
+                    Image("profile")
+                }.accentColor(Color("black400")),
+            trailing:
+                Button(action: {
+                    print("Notifications")
+                }) {
+                    Image("notificationbell")
+                }.accentColor(Color("black-200"))
+            )
+                .navigationBarTitle("Home", displayMode: .inline)
+        }
     }
 }
 
